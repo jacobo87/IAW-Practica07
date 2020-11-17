33b8322872ca341d39fe40ec3711f76528806f65
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2020-11-17 07:55:21 +0100

    Update balanceador_nginx.sh

--- balanceador_nginx.sh
+++ balanceador_nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# ------------------------------------------------------------------------------ Balanceador Nginx ------------------------------------------------------------------------------ 
-# IPs Servidores Apache
-IPHTTPSERVER1=172.31.88.61
-IPHTTPSERVER2=172.31.90.13
+###########################
+##   Balanceador Nginx  ##
+##########################
+
 # Habilitamos el modo de shell para mostrar los comandos que se ejecutan
 set -x
 cp -r /home/ubuntu/IAW-Practica07/* /home/ubuntu/
@@ -14,4 +14,6 @@ apt-get install nginx -y
 # Movemos el archivo de configuración ya modificado previamente
 cp default /etc/nginx/sites-enabled/
 # Reiniciamos el servicio de nginx
-systemctl restart nginx
+systemctl restart nginx.service
+
+
